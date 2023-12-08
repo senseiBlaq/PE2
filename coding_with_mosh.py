80416e5942ca85c79af5cb0b28149e0ad8e30f70
Paa Kwesi Amamoo <67609834+senseiBlaq@users.noreply.github.com>
2023-12-08 04:31:14 +0000

    Update coding_with_mosh.py with solutions for exercise
    Tutorials is on YouTube programming with Mosh

---
+++ coding_with_mosh.py
@@ -0,0 +1,9 @@
+# Exercise 3
+#Ask user's weight in pounds and convert to KG 
+name = input ('Hi there, whats your name
+? ')
+2 weight= int(input ('Nice to meet you ' + name + '. Tell me your weight in pounds and il'll convert it to kilograms. '))
+weight_kg = weight / 2.205
+4 print ('Your weight in kilogram is ', weight_kg)
+5
+print ('End of program')
