ba76da0a52753bffd480dffd1838d78ea4d28ca3
Paa Kwesi Amamoo <67609834+senseiBlaq@users.noreply.github.com>
2023-12-10 14:10:09 +0000

    Update coding_with_mosh.py with exercise 4

--- coding_with_mosh.py
+++ coding_with_mosh.py
@@ -6,4 +6,27 @@ name = input ('Hi there, whats your name
 weight_kg = weight / 2.205
 4 print ('Your weight in kilogram is ', weight_kg)
 5
+#Exercise 4 
+#Price of a house is $1M. If buyer has good credit, they need to put down 10%Otherwisethey need to put down 20%
+price_house = 1000000
+2
+credit_score = True
+
+3 - if credit_score == True :
+4
+down_payment = price_house * 0.10
+5
+print(f'your down payment is 10% is
+
+{down_payment}')
+
+6 - else :
+7
+down_payment = price * 0.20
+8
+print (f'your down payment is 20% is
+
+{downpayment}')
+9
+print("End of program !")
 print ('End of program')
