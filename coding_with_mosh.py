e71216acf274e6e6683a47501f8a9ad5663db95f
Paa Kwesi Amamoo <67609834+senseiBlaq@users.noreply.github.com>
2023-12-10 14:17:42 +0000

    Update coding_with_mosh.py code clean up

--- coding_with_mosh.py
+++ coding_with_mosh.py
@@ -2,31 +2,20 @@
 #Ask user's weight in pounds and convert to KG 
 name = input ('Hi there, whats your name
 ? ')
-2 weight= int(input ('Nice to meet you ' + name + '. Tell me your weight in pounds and il'll convert it to kilograms. '))
+weight= int(input ('Nice to meet you ' + name + '. Tell me your weight in pounds and il'll convert it to kilograms. '))
 weight_kg = weight / 2.205
-4 print ('Your weight in kilogram is ', weight_kg)
-5
+print ('Your weight in kilogram is ', weight_kg)
+
 #Exercise 4 
 #Price of a house is $1M. If buyer has good credit, they need to put down 10%Otherwisethey need to put down 20%
 price_house = 1000000
-2
 credit_score = True
 
-3 - if credit_score == True :
-4
-down_payment = price_house * 0.10
-5
-print(f'your down payment is 10% is
+if credit_score :
+    down_payment = price_house * 0.10
+else :
+    down_payment = price * 0.20
 
-{down_payment}')
+print(f'your down paymentis{down_payment}')
 
-6 - else :
-7
-down_payment = price * 0.20
-8
-print (f'your down payment is 20% is
-
-{downpayment}')
-9
-print("End of program !")
 print ('End of program')
